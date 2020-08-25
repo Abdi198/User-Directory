77287fa92047f94d052d0ff7f2b40a1a57ac45a6
Abdi Ismail <sessiano@gmail.com>
2020-08-24 21:47:09 -0500

    added process.env

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ var app = express();
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
+  // A
 app.listen(Port, function() {
     console.log("app is listening!" + Port)
 });
